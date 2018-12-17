c226b6878bdcf62a7f6b105bb6b26ebebe5f9d20
Cryptoshot <mannchen.timon@gmail.com>
2018-12-17 09:45:25 +0100

    Anpassungen: StandUp Conf.
    Funktion
    link
    center

--- lhind-agilportal/src/app/components/standup-config/standup-config.component.ts
+++ lhind-agilportal/src/app/components/standup-config/standup-config.component.ts
@@ -9,6 +9,18 @@ import { Component, OnInit} from '@angular/core';
 export class StandupConfigComponent implements OnInit {
   title= "Daily Stand Up";
 
+  formatLabel(value: number | null) {
+    if (!value) {
+      return 0;
+    }
+
+    if (value >= 1) {
+      return Math.round(value / 1);
+    }
+
+    return value;
+  }
+
   constructor() { }
 
   ngOnInit() {
